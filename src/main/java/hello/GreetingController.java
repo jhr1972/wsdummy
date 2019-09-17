25e7e2d17d4b308c4d16a8fac089e735b00d0162
jochen.reitzig <jochen.reitzig@atos.net>
2019-09-17 14:48:32 +0000

    logging added

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,4 +1,6 @@
 package hello;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +12,18 @@ public class GreetingController  {
 
     private static final String template = "Vlanid: %s!";
     private final AtomicLong counter = new AtomicLong();
+    Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
+
+    @RequestMapping("/")
+    public void root(@RequestBody String json) {
+//	logger.debug("Body: " + json);
+        return; 
+    }
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+	logger.debug("in /greeting");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
 
